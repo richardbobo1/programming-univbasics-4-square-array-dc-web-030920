f40bd54e56f4aba0f1062db7aa3f2c06d326cb2e
Richard Bobo <richard.bobo@gmail.com>
2020-02-29 21:18:14 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,7 +4,7 @@ def square_array(numbers)
    counter = 0
  
  while counter < numbers.length do
-   square_array[counter] << numbers[counter]**numbers[counter]
+   square_array[counter] ( numbers[counter]**numbers[counter] )
    counter += 1
  end 
 end
@@ -15,7 +15,7 @@ def square_array(new_numbers)
    counter = 0
 
  while counter < new_numbers.length do
-  square_array.push << new_numbers[counter]**new_numbers[counter]
+  square_array.push(new_numbers[counter]**new_numbers[counter])
    counter += 1
  end 
 end
